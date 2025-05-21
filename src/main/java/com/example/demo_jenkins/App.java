3b5c08c3f60d46997da2564b38aa6a8f6c10b32a
Geetha <danda.saigeetha@wipro.com>
2025-05-21 05:33:45 +0000

    initial commit

---
+++ src/main/java/com/example/demo_jenkins/App.java
@@ -0,0 +1,10 @@
+package com.example.demo_jenkins;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
